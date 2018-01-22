45d4c9085487fc914ca0d18d77fa991c4ec6a7b0
Colin <marshallcolin71@gmail.com>
2018-01-22 16:09:07 -0500

    updated and beautified  user model and spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,30 @@
 class User < ActiveRecord::Base
-  has_many :posts
-  before_save { self.email = email.downcase if email.present? }
-  before_save { self.role ||= :member }
-  before_save :format_name
+	has_many :posts, dependent: :destroy
+	has_many :comments, dependent: :destroy
+	has_many :votes, dependent: :destroy
+	has_many :favorites, dependent: :destroy
 
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-   validates :password, length: { minimum: 6 }, allow_blank: true
-   validates :email,
-             presence: true,
-             uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 254 }
+	before_save { self.email = email.downcase if email.present? }
+	before_save { self.role ||= :member }
 
-   has_secure_password
+	validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+	validates :password, length: { minimum: 6 }, if: "password_digest.nil?"
+	validates :password, length: { minimum: 6 }, allow_blank: true
+	validates :email,
+						presence: true,
+						uniqueness: { case_sensitive: false },
+						length: { minimum: 3, maximum: 254 }
 
-   def format_name
-     if name
-       name_array = []
-       name.split.each do |name_part|
-         name_array << name_part.capitalize
-       end
-       self.name = name_array.join(" ")
-     end
-   end
+	has_secure_password
+
+	enum role: [:member, :admin]
+
+	def favorite_for(post)
+		favorites.where(post_id: post.id).first
+	end
+
+	def avatar_url(size)
+		gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+		"http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+	end
 end
